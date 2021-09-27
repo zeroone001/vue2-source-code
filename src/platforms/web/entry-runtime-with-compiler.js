6cce3aa2035772350c07a7dfb95831430f3b2693
LYS <skylys54@gmail.com>
2021-09-27 14:08:40 +0800

    生命周期，# 模板编译阶段

--- src/platforms/web/entry-runtime-with-compiler.js
+++ src/platforms/web/entry-runtime-with-compiler.js
@@ -19,8 +19,6 @@ const idToTemplate = cached(id => {
 })
 
 const mount = Vue.prototype.$mount
-
-
 Vue.prototype.$mount = function (
   el?: string | Element,
   hydrating?: boolean
@@ -29,9 +27,12 @@ Vue.prototype.$mount = function (
     否则就直接返回
   */
   el = el && query(el)
-
   /* istanbul ignore if */
   /* 不能直接挂载到body或者HTML上上 */
+  /* 
+    Vue模板中的内容将会替换el 对应的DOM元素
+    如果是body或者HTML会破坏文档流
+  */
   if (el === document.body || el === document.documentElement) {
     process.env.NODE_ENV !== 'production' && warn(
       `Do not mount Vue to <html> or <body> - mount to normal elements instead.`
@@ -41,10 +42,8 @@ Vue.prototype.$mount = function (
 
   const options = this.$options
   // resolve template/el and convert to render function
-
-
   /* 
-    如果没有写render
+    如果没有写render,就获取template，编译成render函数
   */
   if (!options.render) {
     let template = options.template
@@ -52,7 +51,10 @@ Vue.prototype.$mount = function (
       /* 如果写了template 那么走下面的 */
       /* 如果手写了，就用下面这个值， 如果没有手写，那么就用el 去获取 */
       if (typeof template === 'string') {
+        /* 如果变量template存在，则接着判断如果template是字符串并且以##开头，
+        则认为template是id选择符 */
         if (template.charAt(0) === '#') {
+          /* 用idToTemplate函数获取到选择符对应的DOM元素的innerHTML作为模板 */
           template = idToTemplate(template)
           /* istanbul ignore if */
           if (process.env.NODE_ENV !== 'production' && !template) {
@@ -63,6 +65,7 @@ Vue.prototype.$mount = function (
           }
         }
       } else if (template.nodeType) {
+        /* 判断是不是一个DOM元素，如果是的话，就返回innerHTML */
         template = template.innerHTML
       } else {
         if (process.env.NODE_ENV !== 'production') {
@@ -71,18 +74,23 @@ Vue.prototype.$mount = function (
         return this
       }
     } else if (el) {
-      // 正常走这个
+      /* 
+        如果没有template这个options
+        那么就根据el ，获取外部模板
+      */
       template = getOuterHTML(el)
     }
+
     if (template) {
-
-      /* istanbul ignore if */
-
+      /* 上面模板已经准备好了，接下来，就是把模板编译成render函数 */
       /* 下面就是关键的编译了 */
       if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
         mark('compile')
       }
-
+      /* 主要在compileToFunctions这个函数中进行的
+        返回render这个函数，
+        然后挂载到options上面  
+      */
       const { render, staticRenderFns } = compileToFunctions(template, {
         outputSourceRange: process.env.NODE_ENV !== 'production',
         shouldDecodeNewlines,
@@ -101,8 +109,8 @@ Vue.prototype.$mount = function (
       }
     }
   }
+  /* mount 就是runtime里面的 $mount */
   return mount.call(this, el, hydrating)
-  
 }
 
 /**
